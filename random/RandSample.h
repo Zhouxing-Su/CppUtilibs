588b847e6752c9581603a152069b66a639a470c5
suzhouxing <suzhouxing@qq.com>
2017-01-03 21:50:23 +0800

    add some document.

--- random/RandSample.h
+++ random/RandSample.h
@@ -13,11 +13,17 @@
 
 namespace szx {
 
+// count | 1 2 3 4 ...  k   k+1   k+2   k+3  ...  n
+// ------|------------------------------------------
+// index | 0 1 2 3 ... k-1   k    k+1   k+2  ... n-1
+// prob. | 1 1 1 1 ...  1  k/k+1 k/k+2 k/k+3 ... k/n
 class RandSample {
 public:
     RandSample(std::mt19937 &randomNumberGenerator, int targetNumber)
         : rgen(randomNumberGenerator), targetNum(targetNumber), pickCount(0) {}
 
+    // return 0 for not picked.
+    // return an integer i \in [1, targetNum] if it is the i_th item in the picked set.
     int isPicked() {
         if ((++pickCount) <= targetNum) {
             return pickCount;
@@ -27,6 +33,8 @@ public:
         }
     }
 
+    // return -1 for no need to replace any item.
+    // return an integer i \in [0, targetNum) as the index to be replaced in the picked set.
     int replaceIndex() {
         if (pickCount < targetNum) {
             return pickCount++;
